3f86472579c07461d5d7aeb948b958bd43f16227
Abdul Samea (abdul.samea@tcs.com) <1457676@tcs.com>
2021-01-24 00:06:01 +0530

    v2

--- portfolio.js
+++ portfolio.js
@@ -1,9 +1,27 @@
-window.onload = function() {
-    console.log('aaa');
+jQuery(document).ready(function () {
+
+    // get my experience
     var now  = new Date();
     var today = moment([now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate()]);;
     var careerStartDate = moment([2018, 09, 17]);
     var totalExperience = moment.duration(today.diff(careerStartDate));
     var totalExperienceFormatted = totalExperience.years()  + "." + totalExperience.months();
     document.getElementsByClassName("experience-number")[0].innerHTML = totalExperienceFormatted;
+
+function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
 }
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
+
+});
