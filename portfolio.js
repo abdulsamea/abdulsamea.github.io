21ace76297418f24e365ff5c8df74567d3a23b44
Abdul Samea <abdulsamea2@gmail.com>
2021-01-29 17:13:40 +0530

    date fix

--- portfolio.js
+++ portfolio.js
@@ -17,8 +17,8 @@ window.onload = function() {
 
     // get my experience
     var now  = new Date();
-    var today = moment([now.getUTCFullYear(), now.getUTCMonth() + 1, now.getUTCDate()]);;
-    var careerStartDate = moment([2018, 09, 17]);
+    var today = moment([moment().year(), moment().month() , moment().date()]);;
+    var careerStartDate = moment([2018, 08, 17]);
     var totalExperience = moment.duration(today.diff(careerStartDate));
     var totalExperienceFormatted = totalExperience.years()  + "." + totalExperience.months();
     document.getElementsByClassName("experience-number")[0].innerHTML = totalExperienceFormatted;
